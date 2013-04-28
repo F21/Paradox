d45c0ea9c183ab3b73ac950beb9c783a5ff062a5
Francis Chuang <francis.chuang@gmail.com>
2013-04-28 10:42:31 +1000

    Formatting.

--- Paradox/toolbox/GraphManager.php
+++ Paradox/toolbox/GraphManager.php
@@ -45,9 +45,10 @@ class GraphManager
     public function createGraph($name)
     {
         try {
-        	$graph = new \triagens\ArangoDb\Graph($name);
-        	$graph->setVerticesCollection($name . 'VertexCollection');
-        	$graph->setEdgesCollection($name . 'EdgeCollection');
+            $graph = new \triagens\ArangoDb\Graph($name);
+            $graph->setVerticesCollection($name . 'VertexCollection');
+            $graph->setEdgesCollection($name . 'EdgeCollection');
+
             return $this->_toolbox->getGraphHandler()->createGraph($graph);
 
         } catch (\Exception $e) {
@@ -230,12 +231,12 @@ class GraphManager
 
     /**
      * Function to generate the array of filters that will be sent to the server.
-     * @param string $direction  The direction we wish to filter: "any", "in", "out".
-     * @param string $label      A string representing one label or an array of labels we want the inbound edges to have.
-     * @param string $properties An array of property filters. Each filter is an associative array with the following properties:
-     *                              'key' - Filter the result vertices by a key value pair.
-     *                              'value' -  The value of the key.
-     *                              'compare' - A comparison operator. (==, >, <, >=, <= )
+     * @param string       $direction  The direction we wish to filter: "any", "in", "out".
+     * @param array|string $label      A string representing one label or an array of labels we want the inbound edges to have.
+     * @param string       $properties An array of property filters. Each filter is an associative array with the following properties:
+     *                                     'key' - Filter the result vertices by a key value pair.
+     *                                     'value' -  The value of the key.
+     *                                     'compare' - A comparison operator. (==, >, <, >=, <= )
      * @return array
      */
     protected function generateFilter($direction = null, $labels = null, $properties = null)
