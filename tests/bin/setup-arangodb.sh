87686fed971ce44fa52f878c07f423c9bab0fd04
Francis Chuang <francis.chuang@gmail.com>
2013-05-02 09:50:27 +1000

    Fixed shell script.

--- tests/bin/setup-arangodb.sh
+++ tests/bin/setup-arangodb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [$TRAVIS_BRANCH == "devel"]; then
+if [ $TRAVIS_BRANCH == "devel" ]; then
   sudo wget http://www.arangodb.org/repositories/devel/xUbuntu_12.04/Release.key
   sudo apt-key add - < Release.key  
   sudo echo 'deb http://www.arangodb.org/repositories/devel/xUbuntu_12.04/' >> /etc/apt/sources.list.d/arangodb.list
-elif [$TRAVIS_BRANCH == "master"]; then
+elif [ $TRAVIS_BRANCH == "master" ]; then
   sudo wget http://www.arangodb.org/repositories/devel/xUbuntu_12.04/Release.key
   sudo apt-key add - < Release.key  
   sudo echo 'deb http://www.arangodb.org/repositories/devel/xUbuntu_12.04/' >> /etc/apt/sources.list.d/arangodb.list
